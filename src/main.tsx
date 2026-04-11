97464b1932a97cd292df4b8a3e23ed2d0a0b2c81
gustavo1902 <gustavomorais1009@gmail.com>
2026-04-11 10:35:41 -0300

    refactor(main): remover importações desnecessárias e comentários

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
 
-// Importe o Provider e o Client
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
-// Crie uma instância do cliente
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
